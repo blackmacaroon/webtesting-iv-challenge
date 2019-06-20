6fa125046c4c41d8e0b7df90ba4b4ec7d688a73c
Kayla <6lackmacaroon@gmail.com>
2019-06-20 13:39:57 -0600

    three passing tests for server

---
+++ api/server.spec.js
@@ -0,0 +1,26 @@
+const server = require('./server.js');
+const supertest = require('supertest');
+
+describe('server', () => {
+      describe('GET/', () => {
+            it('should respond with 200 OK', () => {
+                  return supertest(server)
+                  .get('/')
+                  .expect(200)
+            })
+            it('should respond with content-type = json', () => {
+                  return supertest(server)
+                  .get('/')
+                  .expect('Content-Type', /json/i);
+            })
+            it('should respond with a message: cutesy movie quote', async () => {
+                  await supertest(server)
+                        .get('/')
+                        .then(res => {
+                              expect(res.body).toEqual({ message: 'look what you did, you little jerk.' })
+                        })
+            })
+
+
+      })
+})
